e5af77efcb200c219cc7530109b18c930536a8c6
Ladno <xmeloff12326@gmail.com>
2023-11-18 16:12:35 +0300

    finally installed libraries

--- prj.cw/material_lib/test.cpp
+++ prj.cw/material_lib/test.cpp
@@ -1,5 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
 #include <GLFW/glfw3.h>
-//#include <gl/glew.h>
+#include <bgfx/platform.h>
+//#include <gl
 
 int main() {
     return 0;
