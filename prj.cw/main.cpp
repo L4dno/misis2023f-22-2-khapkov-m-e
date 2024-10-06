a3d0e25176be5952c157a0b8462991e789120198
Ladno <xmeloff12326@gmail.com>
2024-10-06 15:56:34 +0300

    finished building stage except dll link

--- prj.cw/main.cpp
+++ prj.cw/main.cpp
@@ -1,6 +1,28 @@
 
+
 #include "material_lib/include/renderer/renderer.hpp"
 
+#include <vector>
+#include <string>
+
+
+// test app
+
 int main() {
+
+    Renderer r;
+
+    std::vector<std::string> textures =
+    { "../resources/grass.png", "../resources/sand.png", 
+        "../resources/dirt.png", "../resources/snow.png"};
+
+    for (int i = 0; i < 4; ++i)
+        r.SetTextureI(i, textures[i]);
+    r.SetMask("../resources/painting.png");
+
+    r.DrawSelf();
+
+    
+
     return 0;
 }
