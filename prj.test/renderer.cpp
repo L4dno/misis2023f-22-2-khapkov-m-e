0e2628f23efb3980dbaac9a027db3d3704473814
Ladno <xmeloff12326@gmail.com>
2024-10-06 20:44:51 +0300

    test bad shaders

--- prj.test/renderer.cpp
+++ prj.test/renderer.cpp
@@ -11,6 +11,56 @@
 
 #define GLSL_VERSION            330
 
+
+const char* vertexSource = "#version 330\n"
+"in vec3 vertexPosition;"
+"in vec2 vertexTexCoord;"
+"in vec2 vertexTexCoord2;"
+"in vec3 vertexNormal;"
+"in vec4 vertexColor;"
+"uniform mat4 mvp;"
+"uniform mat4 matModel;"
+"uniform mat4 matNormal;"
+"out vec3 fragPosition;"
+"out vec2 fragTexCoord;"
+"out vec2 fragTexCoord2;"
+"out vec4 fragColor;"
+"out vec3 fragNormal;"
+"void main()"
+"{"
+"    fragPosition = vec3(matModel * vec4(vertexPosition, 1.0));"
+"    fragTexCoord = vertexTexCoord;"
+"    fragTexCoord2 = vertexTexCoord2;"
+"    fragColor = vertexColor;"
+"    fragNormal = normalize(vec3(matNormal * vec4(vertexNormal, 1.0)));"
+"    gl_Position = mvp * vec4(vertexPosition, 1.0);"
+"}\n";
+const char* fragmentSource = "#version 330\n"
+"in vec3 fragPosition;"
+"in vec2 fragTexCoord;"
+"in vec2 fragTexCoord2;"
+"in vec4 fragColor;"
+"in vec3 fragNormal;"
+"uniform sampler2D texture0;"
+"uniform sampler2D texture1;"
+"uniform sampler2D texture2;"
+"uniform sampler2D texture3;"
+"uniform sampler2D mask0;"
+"uniform vec4 colDiffuse;"
+"out vec4 finalColor;"
+"void main()"
+"{"
+"    vec4 mask = texture(mask0, fragTexCoord2);"
+"    vec4 baseColor = texture(texture0, fragTexCoord);"
+"    vec4 layer1Color = texture(texture1, fragTexCoord);"
+"    vec4 texelColor = mix(layer1Color, baseColor, mask.r);"
+"    vec4 layer2Color = texture(texture2, fragTexCoord);"
+"    texelColor = mix(layer2Color, texelColor, mask.g);"
+"    vec4 layer3Color = texture(texture3, fragTexCoord);"
+"    texelColor = mix(layer3Color, texelColor, mask.b);"
+"    finalColor = texelColor;"
+"}";
+
 // a simple class to help build up faces of a cube
 class GeomtryBuilder
 {
@@ -294,7 +344,7 @@ void Renderer::DrawSelf() {
     wireMat.maps[MATERIAL_MAP_ALBEDO].color = BLACK;
 
     // load basic lighting
-    Shader lightShader = LoadShaderFromMemory(vertex,fragment);
+    Shader lightShader = LoadShaderFromMemory(vertexSource, fragmentSource);
     //Shader lightShader = LoadShader("", "");
     lightShader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(lightShader, "viewPos");
 
