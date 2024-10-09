ca97b35f4a0191951c5c64b0dba552196190723d
Ladno <xmeloff12326@gmail.com>
2024-10-09 03:58:24 +0300

    final?

---
+++ .build.bat
@@ -0,0 +1,6 @@
+﻿:: вставьте путь до файла vcpkg.cmake после знака '='
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=
+cmake --build build --config release
+
+:: вставьте относительный путь до желаемой директории для установки приложения вместо ./CW
+cmake --install ./build --prefix ./CW
